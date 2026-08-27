f04f266bf18bb419987ac3fe3ea25ca7ddbda991
gui-reinheimer <guilhermereinheimer9636@gmail.com>
2026-08-27 17:04:02 -0300

    Create Maths.java

---
+++ src/operadoresAritmeticos/Maths.java
@@ -0,0 +1,30 @@
+package operadoresAritmeticos;
+
+public class Maths {
+    public static void main(String[] args) {
+        int numero = 25;
+        float quadrada = (float) Math.sqrt(numero); // Raiz Quadrada
+        float Exponente = (float) Math.pow(quadrada, 2); // Exponenciação
+        float cubica = (float) Math.cbrt(numero); //Raiz cubica
+        System.out.println("Raiz quadrada: " + quadrada);
+        System.out.println("Exponente: " + Exponente);
+        System.out.println("Raiz cubica: " + cubica);
+
+        /* ARREDONDAMENTOS */
+        float vlAbsoluto = (float) Math.abs(-10);
+        float arrBaixo = (float) Math.floor(10.9);
+        float arrCima = (float) Math.ceil(10.2);
+        float arrAritmetico = (float) Math.round(8.7);
+        System.out.println("Valor Absoluto: " + vlAbsoluto);
+        System.out.println("Valor arredondado para Baixo: " + arrBaixo);
+        System.out.println("Valor arredondado para Cima: " + arrCima);
+        System.out.println("Valor arredondado Aritimeticamente: " + arrAritmetico);
+
+        /*RAMDOM*/
+
+        double ale = Math.random();
+        int n = (int) (5 + ale * (10-5));
+        System.out.println("Numero Aleatorio: " + ale);
+        System.out.println("Numero Aleatorio: " + n);
+    }
+}
