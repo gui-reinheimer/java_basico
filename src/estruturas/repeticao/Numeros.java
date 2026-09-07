ddf01db2e9d7a99e8fe19a03d3811ae042b4e035
gui-reinheimer <guilhermereinheimer9636@gmail.com>
2026-09-07 12:33:40 -0300

    Create Numeros.java

---
+++ src/estruturas/repeticao/Numeros.java
@@ -0,0 +1,20 @@
+package estruturas.repeticao;
+
+import java.util.Scanner;
+
+public class Numeros {
+    public static void main(String[] args) {
+        int n, s = 0;
+        String resp;
+        Scanner teclado = new Scanner(System.in);
+
+        do {
+            System.out.print("Digite um numero: ");
+            n = teclado.nextInt();
+            s += n;
+            System.out.print("Quer continuar? [S/N] ");
+            resp = teclado.next();
+        } while (resp.equalsIgnoreCase("S"));
+        System.out.println("A soma de todos os valore é: " + s);
+    }
+}
