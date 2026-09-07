97a784403ea81f8f1c8cfcab84b2fd8e6cc24eaf
gui-reinheimer <guilhermereinheimer9636@gmail.com>
2026-09-07 12:17:35 -0300

    Create RepeticaoTesteNoFinal.java

---
+++ src/estruturas/repeticao/RepeticaoTesteNoFinal.java
@@ -0,0 +1,12 @@
+package estruturas.repeticao;
+
+public class RepeticaoTesteNoFinal {
+     public static void main(String[] args) {
+        int cc = 1;
+
+        do {
+            System.out.println("Cambalhota " + cc + "°");
+            cc++;
+        } while (cc <= 4);
+    }
+}
