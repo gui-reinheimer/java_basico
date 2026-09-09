6f4ecd0bf7e2dac3bfe526a1fbde8104d26d8968
gui-reinheimer <guilhermereinheimer9636@gmail.com>
2026-09-09 10:40:50 -0300

    Update ContaNumeros.java

--- src/estruturas/repeticao/ContaNumeros.java
+++ src/estruturas/repeticao/ContaNumeros.java
@@ -6,7 +6,8 @@ public class ContaNumeros {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numDigitado, totalNum = 0, numPar=0, numImpar=0, numAcimaDeCem = 0;
-
+        float media = 0;
+        int soma = 0;
         do {
             System.out.print("Digite um numero (ou 0 para sair): ");
             numDigitado = input.nextInt();
@@ -21,14 +22,15 @@ public class ContaNumeros {
                 if (numDigitado > 100) {
                     numAcimaDeCem++;
                 }
+                soma += numDigitado;
+                media = (float) soma / totalNum;
             }
-
-
         } while (numDigitado != 0);
         System.out.println("Você digitou 0. Encerrando...");
         System.out.println("Total de numeros: " + totalNum);
         System.out.println("Total de pars: " + numPar);
         System.out.println("Total de impar: " + numImpar);
         System.out.println("Acima de 100: " + numAcimaDeCem);
+        System.out.println("A media dos valores: " + media);
     }
 }
